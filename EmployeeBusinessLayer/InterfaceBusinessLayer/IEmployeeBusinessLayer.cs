8017d41c3e2dd75e6bee83de5bd5e2a7851bb2f8
Deepak-Byte <56863853+Deepak-Byte@users.noreply.github.com>
2020-01-07 05:41:08 -0800

    PartOfEmployeeManagement

---
+++ EmployeeBusinessLayer/InterfaceBusinessLayer/IEmployeeBusinessLayer.cs
@@ -0,0 +1,50 @@
+﻿//----------------------------------------------------------------------------
+// <copyright file="IEmployeeBusinessLayer.interface" company="BasicPrograms">
+// Company copyright tag.
+// </copyright>
+//----------------------------------------------------------------------------
+namespace EmployeeBusinessLayer.InterfaceBusinessLayer
+{
+    using EmployeeCommanLayer.EmployeeModel;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This is Interface of EmployeeBusiness Layer.
+    /// </summary>
+    public interface IEmployeeBusinessLayer
+    {
+        /// <summary>
+        /// This is Add Employee method.
+        /// </summary>
+        /// <param name="employeeModels"></param>
+        /// <returns></returns>
+        bool AddEmployee(EmployeeModel employeeModels);
+        /// <summary>
+        /// This is Display all Employee  method.
+        /// </summary>
+        /// <returns></returns>
+        IList<EmployeeModel> DisplayEmployee();
+        /// <summary>
+        /// This is Display Employee by ID method.
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        EmployeeModel DisplayEmployeebyID(int ID);
+        /// <summary>
+        /// This is Update Employee method.
+        /// </summary>
+        /// <param name="employeeModels"></param>
+        /// <returns></returns>
+        bool UpdateEmployee(EmployeeModel employeeModels);
+        /// <summary>
+        /// This is Empoyee Delete method.
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        bool DeleteEmployee(int ID);
+
+    }
+}
---
+++ EmployeeBusinessLayer/ServiceBusinessLayer/EmployeeBusiness.cs
@@ -0,0 +1,110 @@
+﻿//----------------------------------------------------------------------------
+// <copyright file="EmployeeBusiness.cs" company="BasicPrograms">
+// Company copyright tag.
+// </copyright>
+//----------------------------------------------------------------------------
+namespace EmployeeBusinessLayer.ServiceBusinessLayer
+{
+    using EmployeeBusinessLayer.InterfaceBusinessLayer;
+    using EmployeeCommanLayer.EmployeeModel;
+    using EmployeeRepositoryLayer.RepositoryInterface;
+    using EmployeeRepositoryLayer.RepositoryService;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    /// <summary>
+    /// This is Employee Business Layer class which contains all logical part of program.
+    /// </summary>
+    /// <seealso cref="EmployeeBusinessLayer.InterfaceBusinessLayer.IEmployeeBusinessLayer" />
+    public class EmployeeBusiness : IEmployeeBusinessLayer
+    {
+        /// <summary>
+        /// The employee repository
+        /// </summary>
+        private IEmployeeRepositoryLayer employeeRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmployeeBusiness"/> class here cunstructor.
+        /// In EmployeeBusiness Layer constructor initialization of IEmployeeRepositoryLayer.
+        /// Also known as constructor injection.
+        /// </summary>
+        /// <param name="iemployeeRepository">The iemployee repository.</param>
+        public EmployeeBusiness(IEmployeeRepositoryLayer iemployeeRepository)
+        {
+            this.employeeRepository = iemployeeRepository;
+        }
+        /// <summary>
+        /// This is Add Employee method of IEmployeeBusinessLayer.
+        /// </summary>
+        /// <param name="employeeModels"></param>
+        /// <returns></returns>
+        public bool AddEmployee(EmployeeModel employeeModels)
+        {
+            if (employeeModels != null)
+            {
+                bool addresult =  this.employeeRepository.AddEmployee(employeeModels);
+                return addresult;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// This is Empoyee Delete method  of IEmployeeBusinessLayer.
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        public bool DeleteEmployee(int ID)
+        {
+            if (ID != 0)
+            {
+                bool deleteresult = this.employeeRepository.DeleteEmployee(ID);
+                return deleteresult;
+            }
+            return false;
+        }
+        /// <summary>
+        /// This is Display all Employee  method of IEmployeeBusinessLayer.
+        /// </summary>
+        /// <returns></returns>
+        public IList<EmployeeModel> DisplayEmployee()
+        {
+            return this.employeeRepository.DisplayEmployee();
+        }
+        /// <summary>
+        /// This is Update Employee method of IEmployeeBusinessLayer.
+        /// </summary>
+        /// <param name="employeeModels"></param>
+        /// <returns></returns>
+        public bool UpdateEmployee(EmployeeModel employeeModels)
+        {
+            if (employeeModels != null)
+            {
+                bool updateresult = employeeRepository.UpdateEmployee(employeeModels);
+                return updateresult;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// This is Display Employee by ID method of IEmployeeBusinessLayer.
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        public EmployeeModel DisplayEmployeebyID(int ID)
+        {
+            if (ID != 0)
+            {
+                EmployeeModel displaydata = employeeRepository.DisplayEmployeebyID(ID);
+                return displaydata;
+            }
+            return null;
+        }
+    }
+}
